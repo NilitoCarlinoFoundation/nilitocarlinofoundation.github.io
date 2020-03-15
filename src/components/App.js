d6d726a5263837f35483cec5d02d7b8564b3f6ff
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2020-03-15 12:00:34 -0400

    Added new image files as well as fontawesome code to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 
+/* Here's where I can grab FB and star icons*/
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
+/*end of FB and star icons*/
+
 function App() {
   return (
     <div className="App">
