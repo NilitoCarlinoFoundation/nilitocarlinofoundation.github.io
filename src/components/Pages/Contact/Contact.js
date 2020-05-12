c737b69ddcef930579881bcc0cae2a339002ca36
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2020-05-12 17:50:34 -0400

    Update contribors and sponsors spacing

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export default function Contact() {
-	return ( 
-		<div>Contact</div>
-	)
+  return (
+    <div className="Contact">
+
+      <div className="content">
+        <h1>Contact Us</h1>
+        <div className="contact-us">
+          Here is the contact form
+        </div>
+      </div>
+
+    </div>
+  );
 }
--- src/components/Pages/Events/Events.js
+++ src/components/Pages/Events/Events.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 import './Calendar.js';
+import './Events.css';
 
 export default function Events() {
-	return ( 
-		<div>Events</div>
-	)
+  return (
+    <div className="Events">
+
+      <div className="content">
+        <h1>Calendar</h1>
+        <div className="calendar">
+  {/*        <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js"></script>*/}
+        </div>
+      </div>
+
+      <div className="content">
+        <h1>Upcoming Events</h1>
+        <div className="content-text">
+          Date on one side, details on the other with Register button
+        </div>
+      </div>
+
+      <div className="content">
+        <h1>Past Events</h1>
+        <div className="content-text">
+          Date on one side, details on the other with Results button
+        </div>
+      </div>
+    </div>
+  );
 }
