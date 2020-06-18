f4bf221ffb2e33a86619cde462259d1f55ad35ef
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2020-06-18 18:48:25 -0400

    Update contact us language

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <div className="content">
         <h1>Contact Us</h1>
         <div className="content-text">
-          We are excited to hear from you and welcome your questions, comments, and feedback about the foundation and our
+          We are excited to hear from you and welcome your questions and feedback about the foundation and our
           website. Feel free reach out to us at:{" "}
           {/*<br />*/}
           <a className="text-link" href="mailto:rrcarlino3@gmail.com">The Nilito Carlino Foundation</a>
