12a62b6c3e317ff39109b3e6022aab7f3ad4b456
Peter Grippi <peter@petergrippi.com>
2020-06-21 14:14:05 -0400

    Updates

---
+++ precache-manifest.8d27f0dc7d0491700ac68f6550fec40b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cac14d6982a8ea3d9bb751d9f338b7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a39ec074dab275dcdc54",
+    "url": "/static/css/main.1e589fdb.chunk.css"
+  },
+  {
+    "revision": "4a13a3ac98c2b2724662",
+    "url": "/static/js/2.b777610f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b777610f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39ec074dab275dcdc54",
+    "url": "/static/js/main.a5efe263.chunk.js"
+  },
+  {
+    "revision": "94133e5a637849f875fc",
+    "url": "/static/js/runtime-main.b49409a2.js"
+  }
+]);
