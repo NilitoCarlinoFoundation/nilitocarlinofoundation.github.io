dd137e3187dc98e66a91ebe8186738fbba5a99ae
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2021-05-09 13:28:26 -0400

    Add silver and bronze sponsors

---
+++ src/components/Pages/Sponsors/Sponsor.js
@@ -0,0 +1,21 @@
+/*import React from 'react';
+//import './Sponsors.js';
+
+
+$(function Sponsor(){
+  var tickerLength = $('.Sponsors-list ul li').length;
+  var tickerHeight = $('.Sponsors-list ul li').outerHeight();
+  $('.Sponsors-list ul li:last-child').prependTo('.Sponsors-list ul');
+  $('.Sponsors-list ul').css('marginTop',-tickerHeight);
+  function moveTop(){
+    $('.Sponsors-list ul').animate({
+      top : -tickerHeight
+    },600, function(){
+     $('.Sponsors-list ul li:first-child').appendTo('.Sponsors-list ul');
+      $('.Sponsors-list ul').css('top','');
+    });
+   }
+  setInterval( function(){
+    moveTop();
+  }, 3000);
+  });*/
