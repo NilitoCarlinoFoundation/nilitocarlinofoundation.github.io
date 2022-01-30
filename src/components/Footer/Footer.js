35b450a3c66671686b402c673f546949c672abdc
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2022-01-30 09:55:50 -0500

    update copyright year. unsure what one.js is

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
       <div>
         The Nilito Carlino Foundation | 702 Oakhill Ave, Attleboro, MA 02703
       </div>
-      <div>Copyright &#169; 2021</div>
+      <div>Copyright &#169; 2022</div>
     </div>
   );
 }
