69a445d00cd299a1c258a92b4fc61b650956394e
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2025-04-16 05:58:43 -0400

    update foundation address

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="Footer">
       <div>
-        The Nilito Carlino Foundation | 702 Oakhill Ave, Attleboro, MA 02703
+        The Nilito Carlino Foundation | PO Box 87, Attleboro, MA 02703
       </div>
       <div>Copyright &#169; 2021-2025</div>
     </div>
