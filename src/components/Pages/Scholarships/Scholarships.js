8c0606602de3e0fd2b906b6798e1232ffe9fe78d
Sharon Lecuyer <sharon.lecuyer@gmail.com>
2025-05-17 07:29:09 -0400

    add 2025 scholarship recipient names and photo

--- src/components/Pages/Scholarships/Scholarships.js
+++ src/components/Pages/Scholarships/Scholarships.js
@@ -1,29 +1,32 @@
 import React from 'react';
 
-import FirstScholarship from '../../../assets/images/ScholarshipRecipientYearTwo.png';
+import Scholarship from '../../../assets/images/Scholarships2025.jpeg';
 import './Scholarships.css';
 
 export default function Story() {
   return (
     <div className="Scholarships">
       <div className="content-image">
-        <img src={FirstScholarship} alt="Second Scholarship Recipient" />
+        <img src={Scholarship} alt="2025 Scholarship Recipients" />
       </div>
 
       <div className="content">
         <h1>Scholarship Recipients</h1>
         <div className="content-text">
-         $2,500 scholarship recipients: Alexis MacDonald and Miracle Arce
+         $2,500 scholarship recipients: Sophia Mastandreau and Sophia Correia
           <p>
-          We are so pleased to announce that the foundation awarded <i>two</i> scholarships this season on May 18th, 2023 at Attleboro High School
-          to Alexis MacDonald and Miracle Arce who will be attending Johnson & Wales University to study Bakery/Pastry in the fall.
-          Congratulations, Alexis and Miracle!
+          We are thrilled to announce that on May 15, 2025, the foundation awarded <i>two</i> scholarships at Attleboro High School.
+          This season's recipients are Sophia Mastandreau and Sophia Correia, who will both be attending Johnson & Wales University in the fall.
+          Congratulations, to you both!
           </p>
           <p>
           <b>Past Recipients:</b>
           </p>
           <p>
-          2022 - Felicity Norton: $2500, Johnson & Wales University
+            <ul>
+              <li>2022 - Felicity Norton: $2500, Johnson & Wales University</li>
+              <li>2023 - Alexis MacDonald and Miracle Arce: $2500 each, Johnson & Wales University</li>
+            </ul>
           </p>
         </div>
       </div>
